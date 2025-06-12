4e2ae1e6e87f09b4cdf75662a2326f011f624c22
khanhnguyen2001 <nqkhanh167@gmail.com>
2025-06-12 23:31:36 +0700

    hehe

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -8,9 +8,12 @@ const isAdminOrLibrarian = require('../middlewares/isAdminOrLibrarian');
 
 // Lấy thông tin người dùng (cần đăng nhập)
 router.get('/me', authenticateToken, userController.getUserInfo);
+// Tạo tài khoản người dùng
+router.put('/create', authenticateToken, isAdminOrLibrarian, userController.createUser);
 
 // Cập nhật thông tin người dùng
 router.put('/me', authenticateToken, userController.updateUserSelf);
+router.put('/:id', authenticateToken, isAdminOrLibrarian, userController.updateUserByAdmin);
 router.put('/:id', authenticateToken, isAdminOrLibrarian, userController.updateUserByAdmin);
 
 module.exports = router;
